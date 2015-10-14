292899c9c2e54834d90b601c2ae73a67613e3026
Nguyen Quang Hien <quanghien95@gmail.com>
2015-10-14 10:17:21 +0700

    Thực tập html
    Thực tập html

---
+++ custom.js
@@ -0,0 +1,34 @@
+// $('a[href*=#]:not([href=#])').not('#myCarousel a, .modal-trigger a, .panel a').click(function(o){
+// 	if (localtion.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || localtion.hostname == this.hostname){
+// 		var target = $(this.hash);
+//       	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       	if (target.length) {
+//         	if ($(".navbar").css("position") == "fixed"){
+//         		$('html,body').animate({
+//         			scrollTop: target.offset().top - 72
+//         		}, 700, 'swing');
+//         	} else{
+//         		$('html,body').animate({
+//         			scrollTop: target.offset().top
+//         		}, 700, 'swing');
+//         	}
+//         	return false;
+//      	}	
+// 	}
+// });
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -72
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
